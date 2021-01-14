0d4e0e9ea5e6abfef1a2be0b8567f2c3d3be8fb5
David <dawid.prosba@gmail.com>
2021-01-14 21:59:51 +0100

    Zadanie 1 - library and tests.

--- TempElementsFramework/TempElementsConsoleApp/Program.cs
+++ TempElementsFramework/TempElementsConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TempElementsLib.src.classes;
 
 namespace TempElementsConsoleApp
 {
@@ -6,7 +7,66 @@ namespace TempElementsConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program.Zad1CheckUsing();
+
+            Program.Zad1CheckWithTryAndCatch();
+
+        }
+
+        #region USING
+        static void Zad1CheckUsing()
+        {
+
+            Program.DisplayBeginTestLine("Zad 1Check using");
+
+            using (TempFile file = new TempFile())
+            {
+                Console.WriteLine("Sprawdz czy plik się utworzył");
+                Console.ReadLine();
+                Console.WriteLine("Sprawdz czy dane wpisały się do pliku");
+
+                file.AddText("[1]");
+
+                Console.ReadLine();
+                Console.WriteLine("W tym momencie plik powinien się usunąć..");
+
+            }
+
+            Program.DisplayEndTestLine();
+
+        }
+        #endregion
+
+        static void Zad1CheckWithTryAndCatch()
+        {
+            Program.DisplayBeginTestLine("Zad1 Check try and catch");
+
+            TempFile file = new TempFile();
+            try
+            {
+                Console.WriteLine("Dodawanie tekstu do pliku, sprawdz czy tekst sie dodał");
+                file.AddText("TEST");
+                Console.ReadLine();
+                Console.WriteLine("Usuwanie pliku, a nastepnie próba dodania tekstu do pliku");
+                file.Dispose();
+
+                file.AddText("Ten tekst nie zostanie dodany");
+
+            }catch(ObjectDisposedException e){
+                Console.WriteLine("Plik został już usunięty, nie można wpisać danych do pliku.");
+            }
+ 
+            Program.DisplayEndTestLine();
+
+        }
+
+        static void DisplayBeginTestLine(string testName)
+        {
+            Console.WriteLine($"-- {testName} --");
+        }
+
+        static void DisplayEndTestLine() { 
+            Console.Write("-- ** --");
         }
     }
 }
