29fc8e4743703cacc5c123a4152826d7115a3bde
David <dawid.prosba@gmail.com>
2021-01-17 12:16:39 +0100

    Move files one level down

---
+++ TempElementsLib/src/classes/TempElementsStack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TempElementsLib.Interfaces;
+
+namespace TempElementsLib.src.classes
+{
+    class TempElementsStack : ITempElements
+    {
+        public IReadOnlyCollection<ITempElement> Elements => throw new NotImplementedException();
+
+        public T AddElement<T>() where T : ITempElement, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveDestroyed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
